de187f60d6481e0e44e813790bb6d3df54bf71e8
Alejandro Parra <pa.parra12@uniandes.edu.co>
2020-03-23 16:38:23 -0500

    Qlearning init and argmax()

--- code/fisr_agent.py
+++ code/fisr_agent.py
@@ -1,12 +1,23 @@
 from agent import BaseAgent
+import numpy as np
 
 
-class TDAgent(BaseAgent):
-    def agent_init(self, agent_info={}):
-        raise NotImplementedError
+class QLearningAgent(BaseAgent):
+
+    def __init__(self, agent_init_info={}):
+
+        self.num_actions = agent_init_info['num_actions']
+        self.num_states = agent_init_info['num_states']
+        self.epsilon = agent_init_info['epsilon']
+        self.step_size = agent_init_info['step_size']
+        self.discount = agent_init_info['discount']
+        self.rand_generator = np.random.RandomState(agent_init_info['seed'])
+
+        # Create an array for action-value estimates and initialize to zero
+        self.q = np.zeros((self.num_states, self.num_actions))
 
     def agent_start(self, state):
-        raise NotImplementedError
+        raise NotImplementedError        
 
     def agent_step(self, reward, state):
         raise NotImplementedError
@@ -19,3 +30,21 @@ class TDAgent(BaseAgent):
 
     def agent_message(self, message):
         raise NotImplementedError
+
+    def argmax(self, q_values):
+        """ argmax with random tie-breaking 
+        :param q_values (numpy arrary): the array of action-values
+        :return action (int): an action with the highest value 
+        """
+
+        top = float('-inf')
+        ties = []
+        for i in range(len(q_values)):
+            if q_values[i] < top: 
+                top = q_values[i]
+                ties = []
+
+            if q_values == top:
+                ties.append(i)
+
+        return self.rand_generator.choice(ties)
