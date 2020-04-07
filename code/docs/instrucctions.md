b79afea7177eaeabcdd75adeb34f9291bb71d006
AP <pa.parra12@uniandes.edu.co>
2020-04-06 22:52:53 -0500

    Report class

--- code/docs/instrucctions.md
+++ code/docs/instrucctions.md
@@ -15,3 +15,10 @@
 - Actions = 160
 - States = 435897
 - Current state = 435986
+
+Para poner una progressbar dentro de otra se puede modificar la posición: 
+
+```python
+pbar1 = tqdm(total=100, position=1)
+pbar2 = tqdm(total=200, position=0)
+```
--- code/main.py
+++ code/main.py
@@ -2,6 +2,8 @@ from fisr_env import FisrEnvironment
 from fisr_agent import QLearningAgent
 from noname import Training
 from noname import Production
+import numpy as np
+import pandas as pd
 
 env = FisrEnvironment()
 agent = QLearningAgent()
@@ -13,3 +15,7 @@ q_values = training.agent.q
 
 production = Production(env, agent, q_values, 0)
 production.run_production(1, 5)
+
+nodes = production.env.system.system_data.nodes
+df = pd.DataFrame(data=nodes, columns=['Nodo fallado'])
+
